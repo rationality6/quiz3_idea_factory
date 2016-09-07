fe40dd81b7a567602d63ae6488f4c231808e9e6d
Hyun Ahn <rationality6@gmail.com>
2016-09-07 08:39:07 -0700

    Comments done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :comment_param
+
   def create
-  @comment = Comment.new params.require(:comment).permit(:body)
-  @post = Post.find params[:post_id]
-  @comment.post = @post
-  if @comment.save
-    redirect_to post_path(@post), notice: 'Answer created!'
-  else
-    render '/posts/show'
+    @post = Post.find params[:post_id]
+    @comment.post = @post
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@post), notice: 'Answer created!' }
+        format.js { render }
+      else
+        format.html { render '/posts/show' }
+        format.js { render }
+      end
+    end
   end
-end
 
-def destroy
-  @answer = Answer.find params[:id]
-  @answer.destroy
-  respond_to do |format|
-    format.html { redirect_to question_path(@answer.question), notice: 'Answer deleted' }
-    format.js   { render } # this renders /app/views/answers/destroy.js.erb
+  def destroy
+    @answer = Answer.find params[:id]
+    @answer.destroy
+    respond_to do |format|
+      format.html { redirect_to question_path(@answer.question), notice: 'Answer deleted' }
+      format.js   { render } # this renders /app/views/answers/destroy.js.erb
+    end
   end
-end
 
+  private
+
+  def comment_param
+    @comment = Comment.new params.require(:comment).permit(:body)
+  end
 end
